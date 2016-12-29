e59d9a16d91650b57a9b0f249500ecd4634aefda
Md. Jahirul Islam Sourav <jahirulsourav@gmail.com>
2016-12-29 18:16:55 +0600

    added a new file with lcd display control
    a 16 x 2 LCD display is added instade of Serial monitor

---
+++ two-ldr-maintance-code-with-16x2-lcd-display/two-ldr-maintance-code-with-16x2-lcd-display.ino
@@ -0,0 +1,44 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int thresholdValue = 500;
+
+byte ldrPin1 = A0;
+byte ldrPin2 = A1;
+
+int count = 0;
+
+int priviousValue1 = 0;
+int priviousValue2 = 0;
+
+bool ldrCheck(int ldrValue){
+  if (ldrValue>=thresholdValue) return true;
+  else return false;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int ldrValue1 = analogRead(ldrPin1);
+  int ldrValue2 = analogRead(ldrPin2);
+  
+  if(ldrCheck(ldrValue1) != ldrCheck(priviousValue1)) count++;
+  if(ldrCheck(ldrValue2) != ldrCheck(priviousValue2)) count--;
+  
+  //Serial.println(count);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.println("WC To CAR contrl");
+  lcd.print("Total Car:  ");
+  lcd.println(count);
+  delay(10);
+  
+  priviousValue1 = ldrValue1;
+  priviousValue2 = ldrValue2;
+}
